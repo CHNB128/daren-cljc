45776d981df60acc8b73b57fa2ff155a9302aaf2
drake.aren <drake.aren@protonmail.com>
2019-11-29 23:25:43 +0600

    add flip fn

--- src/daren_cljc/core.clj
+++ src/daren_cljc/core.clj
@@ -1,6 +1,11 @@
 (ns daren-cljc.core)
 
-(defmacro infer-map [& args]
+(defn flip
+  [f]
+  (fn [& args] (apply f (reverse args))))
+
+(defmacro infer-map
+  [& args]
   {:pre [(every? symbol? args)]}
   `(hash-map
      ~@(interleave
